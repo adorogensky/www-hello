6b0f1b2494badf69b43b5b1cb898c28c61fc7dba
Alex Dorogensky <amazinglifetime@gmail.com>
2020-04-17 16:44:21 -0500

    Remove 'Hello' text and display value state.counter initialized to 0

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
   render() {
     return (
-      <React.Fragment>
-        <h1>Hello!</h1>
+      <div>
+        <span>{this.state.count}</span>
         <button>Increment</button>
-      </React.Fragment>
+      </div>
     );
   }
 }
