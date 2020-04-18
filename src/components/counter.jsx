cf9aacd810d329e9ab7c8e5a8dc9b25727535e69
Alex Dorogensky <amazinglifetime@gmail.com>
2020-04-18 08:47:25 -0500

    Remove style attribute and style variable

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,16 +5,10 @@ class Counter extends Component {
     count: 0,
   };
 
-  style = {
-    fontSize: 50,
-  };
-
   render() {
     return (
       <div>
-        <span style={this.style} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
