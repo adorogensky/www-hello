90bb8416bdbdcd2b39b3c4961abc7ef5940403c9
Alex Dorogensky <amazinglifetime@gmail.com>
2020-04-18 08:45:39 -0500

    Use style attribute with properties from external object for demo

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,10 +5,14 @@ class Counter extends Component {
     count: 0,
   };
 
+  style = {
+    fontSize: 50,
+  };
+
   render() {
     return (
       <div>
-        <span style={{ fontSize: 50 }} className="badge badge-primary m-2">
+        <span style={this.style} className="badge badge-primary m-2">
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
