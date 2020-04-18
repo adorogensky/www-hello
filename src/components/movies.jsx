3833cfd4537b97cd35ad5ba269464777bfa7164c
Alex Dorogensky <amazinglifetime@gmail.com>
2020-04-18 11:23:12 -0500

    { movies: movies } is equivalent to { movies }

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -8,7 +8,7 @@ class Movies extends Component {
 
   handleDelete = (movie) => {
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
-    this.setState({ movies: movies });
+    this.setState({ movies });
   };
 
   render() {
